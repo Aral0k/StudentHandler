8af66a4ff2cc7ab4fe1a402178ea1a90427cf52d
Valerii_kor <myksv200@gmail.com>
2021-08-02 16:59:33 +0400

    02.08 IStudent

---
+++ StudentHandler/StudentHandler.Lib/Students/IStudent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace StudentHandler.Lib.Students
+{
+    public interface IStudent
+    {
+        string Name { get; }
+        IReadOnlyList<int> Marks { get; }
+        double AverageMark { get; }
+        bool IsContractor { get; set; }
+
+        void AddMarks(params int[] marks);
+    }
+}
---
+++ StudentHandler/StudentHandler.Lib/Students/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentHandler.Lib.Students
+{
+    public class Student : IStudent
+    {
+        public string Name => _name;
+        public IReadOnlyList<int> Marks => _marks.AsReadOnly();
+
+        public double AverageMark
+        {
+            get
+            {
+                if (_marks.Count == 0)
+                {
+                    return 0;
+                }
+                return (double) _marks.Sum() / _marks.Count;
+            }
+        }
+
+        public bool IsContractor
+        {
+            get => _isContractor;
+            set => _isContractor = value;
+        }
+
+        private string _name;
+        private List<int> _marks;
+        private bool _isContractor;
+
+        public Student(string name, bool isContractor)
+        {
+            _name = name;
+            IsContractor = isContractor;
+        }
+
+        public void AddMarks(params int[] marks)
+        {
+            if (marks is null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach (var mark in marks)
+            {
+                _marks.Add(mark);
+            }
+        }
+    }
+}
